4045b906a44a0a400be83091509ed4954e3fbf2e
pedro santos <pedroamaralsantos@ua.pt>
2021-04-17 18:55:37 +0200

    updates contract

--- contracts/LetMeIn.sol
+++ contracts/LetMeIn.sol
@@ -3,5 +3,14 @@ pragma solidity ^0.5.0;
 contract LetMeIn {
   uint public providerCount = 0;
 
+  mapping(address => mapping(address => string)) accesses;
+
+  function createAccess(address client, string memory _jsonPermissions) public {
+    accesses[msg.sender][client] = _jsonPermissions;
+  }
+
+  function authorization(address client) public view returns (string memory){
+    return accesses[msg.sender][client];
+  }
 
 }
